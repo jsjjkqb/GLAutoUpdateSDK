a26480fc5df876c28f8e8d3e4028be3c5770eadf
zhaochong <1060787856@qq,com>
2017-04-06 19:54:46 +0800

    兼容Rxjava1

--- GLAutoUpdateSDK/gl_autoupdatesdk/src/main/java/com/ecarx/gl_autoupdatesdk/server/DownloadingService.java
+++ GLAutoUpdateSDK/gl_autoupdatesdk/src/main/java/com/ecarx/gl_autoupdatesdk/server/DownloadingService.java
@@ -6,22 +6,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.ecarx.gl_autoupdatesdk.bean.AppUpdateInfoBean;
 import com.ecarx.gl_autoupdatesdk.config.GLAutoUpdateSetting;
 import com.ecarx.gl_autoupdatesdk.utils.LogTool;
 import com.ecarx.gl_autoupdatesdk.utils.UpdateConstants;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
-import zlc.season.rxdownload2.RxDownload;
-import zlc.season.rxdownload2.entity.DownloadEvent;
-import zlc.season.rxdownload2.entity.DownloadFlag;
-import zlc.season.rxdownload2.entity.DownloadStatus;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import zlc.season.rxdownload.RxDownload;
+import zlc.season.rxdownload.entity.DownloadStatus;
 
 
 /**
@@ -52,7 +48,7 @@ public class DownloadingService extends Service {
     private Context mContext;
     private AppUpdateInfoBean update;
     private String url;
-    private Disposable disposable;
+    private Subscription subscription ;
     private int opState = 0;
     private long percentNumber = 0;
 
@@ -60,7 +56,6 @@ public class DownloadingService extends Service {
     public void onCreate() {
         super.onCreate();
         this.mContext = this;
-        GLAutoUpdateSetting.init(mContext);
     }
 
     @Override
@@ -86,7 +81,7 @@ public class DownloadingService extends Service {
                     DownloadManager.getInstance(mContext).updateNotification(1);
                     break;
                 case UpdateConstants.CANCEL_DOWN:
-                    deleteDownload(url, isDeleteFile());
+                    deleteDownload(url);
                     opState = 2;
                     DownloadManager.getInstance(mContext).updateNotification(2);
                     break;
@@ -105,21 +100,11 @@ public class DownloadingService extends Service {
      */
     private boolean isDownloading(String url) {
         //接收事件可以在任何地方接收，不管该任务是否开始下载均可接收.
-        RxDownload
-                .getInstance(GLAutoUpdateSetting.getInstance().getContext())
-                .receiveDownloadStatus(url)
-                .subscribe(new Consumer<DownloadEvent>() {
-                    @Override
-                    public void accept(DownloadEvent event) throws Exception {
-                        //当事件为Failed时, 才会有异常信息, 其余时候为null.
-                        if (event.getFlag() == DownloadFlag.FAILED) {
-                            Throwable throwable = event.getError();
-                            Log.w("Error", throwable);
-                        }
-                    }
-                });
-
-        return true;
+        if (percentNumber < 100) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -127,8 +112,8 @@ public class DownloadingService extends Service {
      *
      * @param url
      */
-    private void deleteDownload(String url, boolean deleteFile) {
-        RxDownload.getInstance(GLAutoUpdateSetting.getInstance().getContext()).deleteServiceDownload(url, deleteFile).subscribe();
+    private void deleteDownload(String url) {
+        RxDownload.getInstance().deleteServiceDownload(url).subscribe();
     }
 
     /**
@@ -146,7 +131,7 @@ public class DownloadingService extends Service {
      * @param url
      */
     private void pauseDownload(String url) {
-        RxDownload.getInstance(GLAutoUpdateSetting.getInstance().getContext()).pauseServiceDownload(url).subscribe();
+        RxDownload.getInstance().pauseServiceDownload(url).subscribe();
     }
 
     /**
@@ -155,47 +140,41 @@ public class DownloadingService extends Service {
      * @param url
      */
     private void startDownload(String url) {
-        disposable = RxDownload.getInstance(GLAutoUpdateSetting.getInstance().getContext())
-                /**若需要自己的retrofit客户端,可在这里指定*/
-                //   .retrofit(myRetrofit)
-                /**设置默认的下载路径*/
-                .defaultSavePath(GLAutoUpdateSetting.getInstance().getDownloadPath())
+       subscription = RxDownload.getInstance()
                 /**设置最大线程*/
                 .maxThread(3)
                 /**设置下载失败重试次数*/
                 .maxRetryCount(3)
                 /**Service同时下载数量*/
                 .maxDownloadNumber(5)
-                /**只传url即可*/
-                .download(url)
+                .download(url, update.getAppName(), GLAutoUpdateSetting.getInstance().getDownloadPath())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<DownloadStatus>() {
+                .subscribe(new Subscriber<DownloadStatus>() {
                     @Override
-                    public void accept(DownloadStatus status) throws Exception {
-                        /** 解决重复下载字段问题*/
-                        if (percentNumber != status.getPercentNumber()) {
-                            LogTool.d("DownloadStatus为下载进度" + status.getPercentNumber() + "%");
-                            percentNumber = status.getPercentNumber();
-                            if (percentNumber < 100) {
-                                sendBroadcastType(percentNumber);
-                            }
-                        }
-
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        LogTool.d("下载失败" + throwable);
-                        //    glUpdateDownloadCallback.onFail(throwable);
-                    }
-                }, new Action() {
-                    @Override
-                    public void run() throws Exception {
+                    public void onCompleted() {
                         GLAutoUpdateSetting.finshDown = true;
                         sendBroadcastType(100);
                         stopSelf();//停止服务
                         LogTool.d("下载成功");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        LogTool.d("下载失败" + e);
+                        //    glUpdateDownloadCallback.onFail(e);
+                    }
+
+                    @Override
+                    public void onNext(final DownloadStatus status) {
+                        /** 下载状态, 解决重复下载字段问题*/
+                        if (percentNumber != Long.parseLong(status.getPercent())) {
+                            LogTool.d("DownloadStatus为下载进度" + status.getPercent() + "%");
+                            percentNumber = Long.parseLong(status.getPercent());
+                            if (percentNumber < 100) {
+                                sendBroadcastType(percentNumber);
+                            }
+                        }
                     }
                 });
     }
@@ -215,7 +194,7 @@ public class DownloadingService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        disposable.dispose();
+        pauseDownload(url);
         LogTool.d("服务销毁");
     }
 }
