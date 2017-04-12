9a66a4f29ba208c4d9252d0081a27b1a5518c953
zhaochong <1060787856@qq,com>
2017-04-12 09:09:05 +0800

    增加权限监听

---
+++ GLAutoUpdateSDK/gl_autoupdatesdk/src/main/java/com/ecarx/gl_autoupdatesdk/callback/listener/IPermissionListener.java
@@ -0,0 +1,9 @@
+package com.ecarx.gl_autoupdatesdk.callback.listener;
+
+/**
+ * Created by zhaochong on 2017/4/11.
+ */
+
+public interface IPermissionListener<T> {
+    void getPermissionListener(String permission,T t);
+}
---
+++ GLAutoUpdateSDK/gl_autoupdatesdk/src/main/java/com/ecarx/gl_autoupdatesdk/utils/PermissionsUtil.java
@@ -0,0 +1,15 @@
+package com.ecarx.gl_autoupdatesdk.utils;
+
+import android.Manifest;
+
+/**
+ * Created by zhaochong on 2017/4/11.
+ */
+
+public class PermissionsUtil {
+
+    public static final String WRITE_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
+    public static final String READ_EXTERNAL_STORAGE= Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+
+}
